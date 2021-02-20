6ca25c4cb8b4db890fd8e70da0236c726a358f2f
Himanshu <himansingh24@gmail.com>
2021-02-20 11:46:44 +0530

    singalhimanshu: add leetcode solution 102

---
+++ singalhimanshu/leetcode/102.binary-tree-level-order-traversal.cpp
@@ -0,0 +1,51 @@
+// @author: Himanshu
+// @user: singalhimanshu
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+/*
+ * @lc app=leetcode id=102 lang=cpp
+ *
+ * [102] Binary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    if (root == nullptr) {
+      return {};
+    }
+    TreeNode *cur = root;
+    vector<vector<int>> level_order;
+    queue<TreeNode *> q;
+    q.emplace(cur);
+    while (!q.empty()) {
+      size_t q_size = q.size();
+      vector<int> cur_level_val;
+      cur_level_val.reserve(q_size);
+      for (size_t i = 0; i < q_size; i++) {
+        TreeNode *front = q.front();
+        if (front->left)
+          q.emplace(front->left);
+        if (front->right)
+          q.emplace(front->right);
+        cur_level_val.emplace_back(front->val);
+        q.pop();
+      }
+      level_order.emplace_back(std::move(cur_level_val));
+    }
+    return level_order;
+  }
+};
+// @lc code=end
