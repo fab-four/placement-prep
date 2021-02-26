e2677e7431ce743746a5f2472363908438017607
Naveen Rohilla <33195541+RNaveen99@users.noreply.github.com>
2021-02-26 12:00:11 +0530

    RNaveen99: add leetcode solution 142

---
+++ RNaveen99/leetcode/linked-list/142.linked-list-cycle-ii.cpp
@@ -0,0 +1,34 @@
+/**
+ * Author: Naveen Rohilla
+ * User: RNaveen99
+ * Link: https://leetcode.com/problems/linked-list-cycle-ii/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if (slow == fast) {
+                ListNode * temp = head;
+                while (temp != slow) {
+                    temp = temp -> next;
+                    slow = slow -> next;
+                }
+                return temp;
+            }
+        }
+        return NULL;
+    }
+};
