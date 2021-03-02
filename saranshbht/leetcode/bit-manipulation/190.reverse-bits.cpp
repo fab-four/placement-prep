83ebd8391691c31bc97c5c9fa40f8a748b9f645a
Saransh Bhatia <saranshbht@gmail.com>
2021-03-02 21:17:51 +0530

    saranshbht: add leetcode solution 190

---
+++ saranshbht/leetcode/bit-manipulation/190.reverse-bits.cpp
@@ -0,0 +1,27 @@
+/*
+ * @author: Saransh Bhatia
+ * @user  : saranshbht
+ * Link   : https://leetcode.com/problems/reverse-bits/
+ */
+
+class Solution {
+public:
+  uint32_t reverseBits(uint32_t n) {
+    // simple
+    // uint32_t res = 0;
+    // int pos = 32;
+    // while (n) {
+    //     res += ((n & 1) << --pos);
+    //     n >>= 1;
+    // }
+    // return res;
+
+    // brilliant
+    n = (n >> 16) | (n << 16);
+    n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+    n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+    n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+    n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+    return n;
+  }
+};
