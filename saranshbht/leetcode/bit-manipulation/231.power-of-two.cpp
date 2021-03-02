e4996ba4f4aed7ab40b591274247e853e96f1d98
Saransh Bhatia <saranshbht@gmail.com>
2021-03-03 00:42:23 +0530

    saranshbht: edit leetcode solution 231

--- saranshbht/leetcode/bit-manipulation/231.power-of-two.cpp
+++ saranshbht/leetcode/bit-manipulation/231.power-of-two.cpp
@@ -6,5 +6,5 @@
 
 class Solution {
 public:
-  bool isPowerOfTwo(int n) { return (n <= 0 || (n & (n - 1))) ? false : true; }
+  bool isPowerOfTwo(int n) { return (n > 0) && ((n & (n - 1)) == 0); }
 };
