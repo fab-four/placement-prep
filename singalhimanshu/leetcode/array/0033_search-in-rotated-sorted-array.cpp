372a2f24ae6c94b319413b6d7568a61dad173a8d
Himanshu <himansingh24@gmail.com>
2021-04-08 20:03:55 +0530

    singalhimanshu: add leetcode solution 33

---
+++ singalhimanshu/leetcode/array/0033_search-in-rotated-sorted-array.cpp
@@ -0,0 +1,36 @@
+// @author: Himanshu
+// @user: singalhimanshu
+// Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+    int low = 0, high = nums.size() - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[mid] > nums[high]) {
+        if (target >= nums[low] && target < nums[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      } else if (nums[mid] < nums[low]) {
+        if (target > nums[mid] && target <= nums[high]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      } else {
+        if (target <= nums[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      }
+    }
+    return -1;
+  }
+};
