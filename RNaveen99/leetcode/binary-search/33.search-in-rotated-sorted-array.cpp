aa578afe22b649ff2bf24fb87fe0ccbd464e7406
Naveen <naveenrohilla99@gmail.com>
2021-04-16 18:52:52 +0530

    RNaveen99: add leetcode solution 33

---
+++ RNaveen99/leetcode/binary-search/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,72 @@
+/**
+ * Author : Naveen Rohilla
+ * User   : RNaveen99
+ * Link   : https://leetcode.com/problems/search-in-rotated-sorted-array/
+ */
+
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (nums[left] <= target && nums[nums.size() - 1] >= target) {
+            right = nums.size() - 1;
+        } else {
+            right = left;
+            left = 0;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
