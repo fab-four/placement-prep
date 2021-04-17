240db01c3e48f262b5f6266405b67e91ba02d207
Naveen <naveenrohilla99@gmail.com>
2021-04-17 19:11:06 +0530

    RNaveen99: add leetcode solution 235

---
+++ RNaveen99/leetcode/bst/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Author : Naveen Rohilla
+ * User   : RNaveen99
+ * Link   : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+  public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+
+        while (root) {
+            if (root->val > p->val && root->val > q->val) {
+                root = root->left;
+            } else if (root->val < p->val && root->val < q->val) {
+                root = root->right;
+            } else {
+                return root;
+            }
+        }
+        return nullptr;
+    }
+};
