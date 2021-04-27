fede3f9dda54649df230ae388a986d90b349e572
Naveen <naveenrohilla99@gmail.com>
2021-04-27 12:44:29 +0530

    RNaveen99: add leetcode solution 219

---
+++ RNaveen99/leetcode/hash-table/219.contains-duplicate-ii.cpp
@@ -0,0 +1,20 @@
+/**
+ * Author : Naveen Rohilla
+ * User   : RNaveen99
+ * Link   : https://leetcode.com/problems/contains-duplicate-ii/
+ */
+
+class Solution {
+  public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(nums[i]) != m.end()) {
+                if (abs(m[nums[i]] - i) <= k)
+                    return true;
+            }
+            m[nums[i]] = i;
+        }
+        return false;
+    }
+};
