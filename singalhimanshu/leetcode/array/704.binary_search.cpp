0b26e184d0d11ca037258d6a5b860a60080c4fcd
Himanshu <himansingh24@gmail.com>
2021-04-28 21:45:56 +0530

    singalhimanshu: add leetcode solution 704

---
+++ singalhimanshu/leetcode/array/704.binary_search.cpp
@@ -0,0 +1,23 @@
+// @author: Himanshu
+// @user: singalhimanshu
+// Link: https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+  // Time Complexity : O(log(n))
+  // Space Complexity : O(1)
+  int search(vector<int> &nums, int target) {
+    int l = 0, r = nums.size() - 1;
+    while (l <= r) {
+      int mid = (l + r) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+    return -1;
+  }
+};
