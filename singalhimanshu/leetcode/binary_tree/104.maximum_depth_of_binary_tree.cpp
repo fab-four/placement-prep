fd201cd39edec2c2fbf13d9f2a3d7964a1faf0a3
Himanshu <himansingh24@gmail.com>
2021-04-29 13:58:43 +0530

    singalhimanshu: add leetcode solution 104

---
+++ singalhimanshu/leetcode/binary_tree/104.maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,27 @@
+// @author: Himanshu
+// @user: singalhimanshu
+// Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  // Time Complexity : O(n)
+  // Space Complexity : O(log(n))
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr) {
+      return 0;
+    }
+    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+  }
+};
