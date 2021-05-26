a273a91e9fcddd9d4ce94dbd89196c6ed331b8b6
Naveen <naveenrohilla99@gmail.com>
2021-05-26 17:33:59 +0530

    RNaveen99: add leetcode solution 561

---
+++ RNaveen99/leetcode/arrays/561.array-partition-i.cpp
@@ -0,0 +1,17 @@
+/**
+ * Author : Naveen Rohilla
+ * User   : RNaveen99
+ * Link   : https://leetcode.com/problems/array-partition-i/
+ */
+
+class Solution {
+  public:
+    int arrayPairSum(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
