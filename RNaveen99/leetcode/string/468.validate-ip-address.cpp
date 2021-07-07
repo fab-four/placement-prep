e351bb595d40b43932cd515f5058c704ebef659b
Naveen <naveenrohilla99@gmail.com>
2021-07-07 16:55:01 +0530

    RNaveen99: add leetcode solution 468

---
+++ RNaveen99/leetcode/string/468.validate-ip-address.cpp
@@ -0,0 +1,64 @@
+/**
+ * Author : Naveen Rohilla
+ * User   : RNaveen99
+ * Link   : https://leetcode.com/problems/validate-ip-address/
+ */
+
+class Solution {
+  public:
+    string validIPAddress(string IP) { return validIPv4(IP) ? "IPv4" : (validIPv6(IP) ? "IPv6" : "Neither"); }
+
+    bool validIPv4(string IP) {
+        if (count(IP.begin(), IP.end(), '.') != 3) {
+            return false;
+        }
+        vector<string> parts = split(IP, '.');
+        if (parts.size() != 4) {
+            return false;
+        }
+        for (string part : parts) {
+            if (part.empty() || part.size() > 3 || part.size() > 1 && part[0] == '0') {
+                return false;
+            }
+            for (char c : part) {
+                if (!isdigit(c)) {
+                    return false;
+                }
+            }
+            if (stoi(part) > 255) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool validIPv6(string IP) {
+        if (count(IP.begin(), IP.end(), ':') != 7) {
+            return false;
+        }
+        vector<string> parts = split(IP, ':');
+        if (parts.size() != 8) {
+            return false;
+        }
+        for (string part : parts) {
+            if (part.empty() || part.size() > 4) {
+                return false;
+            }
+            for (char c : part) {
+                if (!isdigit(c) && (!isalpha(c) || toupper(c) > 'F')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    vector<string> split(string s, char c) {
+        vector<string> parts;
+        string part;
+        istringstream in(s);
+        while (getline(in, part, c)) {
+            parts.push_back(part);
+        }
+        return parts;
+    }
+};
