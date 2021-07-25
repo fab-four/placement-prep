beec727e5e9dd4895c6038fb7063e4594766c92d
Naveen <naveenrohilla99@gmail.com>
2021-07-25 15:11:30 +0530

    RNaveen99: add/modify

--- RNaveen99/leetcode/linked-list/23.merge-k-sorted-lists.cpp
+++ RNaveen99/leetcode/linked-list/23.merge-k-sorted-lists.cpp
@@ -48,3 +48,49 @@ class Solution {
         return mergedList->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// T : O(N * log(k))
+// S : O(1)
+class Solution {
+  public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if (lists.empty())
+            return NULL;
+        int len = lists.size();
+        while (len > 1) {
+            for (int i = 0; i < len / 2; ++i) {
+                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
+            }
+            len = (len + 1) / 2;
+        }
+
+        return lists.front();
+    }
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *tail = dummy;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tail->next = l1;
+                tail = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                tail = l2;
+                l2 = l2->next;
+            }
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy->next;
+    }
+};
--- RNaveen99/leetcode/linked-list/25.reverse-nodes-in-k-group.cpp
+++ RNaveen99/leetcode/linked-list/25.reverse-nodes-in-k-group.cpp
@@ -46,3 +46,49 @@ class Solution {
         return dummy->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *reverseSublist(ListNode *head, int k) {
+        ListNode *prev = head;
+        ListNode *curr = prev->next;
+
+        ListNode *startNode = curr;
+        while (k--) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        startNode->next->next = prev;
+        startNode->next = curr;
+
+        return startNode;
+    }
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        ListNode *dummy = new ListNode(0, head);
+        ListNode *prev = dummy;
+        int count = 1;
+        while (head) {
+            if (count % k == 0) {
+                prev = reverseSublist(prev, k);
+                head = prev->next;
+                count = 1;
+            } else {
+                head = head->next;
+                count++;
+            }
+        }
+        return dummy->next;
+    }
+};
